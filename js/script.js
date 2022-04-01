18102b6cf8763966846eca9c6cbc2d72c2c85628
hallibentley <hallibentley@gmail.com>
2022-04-01 14:10:41 -0400

    pokemonList array & for loop
    corrected code for pokemon list array & added for loop for name and height

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let pokemonList = [
-  {name =  "Eevee", height = 0.3, type = "normal"};
-  {name =  "Charmander", height = 0.6, type = "fire"};
-  {name =  "Squirtle", height = 0.5, type = "water"};
+  {name:  "Eevee", height: 0.3, type: "normal"},
+  {name:  "Charmander", height: 0.6, type: "fire"},
+  {name:  "Squirtle", height: 0.5, type: "water"}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + pokemonList[i].height) ;
+}
