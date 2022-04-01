18b420ea64ca55e4b2944aa352584ca91901121f
hallibentley <hallibentley@gmail.com>
2022-04-01 14:29:03 -0400

    Pokedex array & height comment
    Organized pokedex array better and added comment for pokemon with a height more than 0.5

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 let pokemonList = [
-  {name:  "Eevee", height: 0.3, type: "normal"},
-  {name:  "Charmander", height: 0.6, type: "fire"},
-  {name:  "Squirtle", height: 0.5, type: "water"}
+  {name:  "Eevee",
+  height: 0.3,
+  type: "normal"},
+
+  {name:  "Charmander",
+  height: 0.6,
+  type: "fire"},
+
+  {name:  "Squirtle",
+  height: 0.5,
+  type: "water"}
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + pokemonList[i].height) ;
+  document.write(pokemonList[i].name + ('Height:') + pokemonList[i].height);
+ if (pokemonList[i].height > 0.5) {
+  document.write('Wow, thats big!');
+}
 }
