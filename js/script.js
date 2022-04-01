06336139908d934c28a0ba00a0e63498a981ccbe
hallibentley <hallibentley@gmail.com>
2022-04-01 12:38:33 -0400

    added pokemon list array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood="blueberries"
-document.write(favoriteFood);
+let pokemonList = [
+  {name =  "Eevee", height = 0.3, type = "normal"};
+  {name =  "Charmander", height = 0.6, type = "fire"};
+  {name =  "Squirtle", height = 0.5, type = "water"};
+];
