ef38dd65ac0cc0e9ec546cacb97bc09fb1727f10
hallibentley <hallibentley@gmail.com>
2022-04-05 13:49:15 -0400

    fixed "getAll" method

--- js/script.js
+++ js/script.js
@@ -29,30 +29,6 @@ let pokemonRepository = (function (){
 
 })();
 
-pokemonRepository.getAll().forEach (function (pokemon)) {
+pokemonRepository.getAll().forEach (function (pokemon) {
   document.write('<br>' + pokemon.name + pokemon.height + pokemon.type + '</br>');
 })
-
-
-
-//pokemonRepository.forEach (function(pokemon)) {
-  //document.write (pokemonList.getAll);
-//};
-
-
-/*
-
-pokemonList.forEach (function(pokemon) {
-  document.write('<br>' + pokemon.name + pokemon.height + pokemon.type + '</br>');
-});
-
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<br>' + pokemonList[i].name + " " +
-  ('Height:') + pokemonList[i].height + " " +
-  ('Type:') + pokemonList[i].type + '</br>');
-
-if (pokemonList[i].height > 0.5) {
-  document.write('Wow, thats big!');
-};
-
-*/
