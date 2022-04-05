b95b8a6c8b0abf8f14979ceea23f33ba26273d64
hallibentley <hallibentley@gmail.com>
2022-04-04 21:26:07 -0400

    Attempted task 5

--- js/script.js
+++ js/script.js
@@ -1,29 +1,52 @@
-let pokemonList = [
-  {name:  "Eevee",
-  height: 0.3,
-  type: ["Normal"] },
+let pokemonRepository = (function (){
 
-  {name:  "Charmander",
-  height: 0.6,
-  type: ["Fire"] },
+  let pokemonList = [
+    {name:  "Eevee",
+    height: 0.3,
+    type: ["Normal"] },
 
-  {name:  "Squirtle",
-  height: 0.5,
-  type: ["Water"] }
-];
+    {name:  "Charmander",
+    height: 0.6,
+    type: ["Fire"] },
 
-/*for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<br>' + pokemonList[i].name + " " +
-  ('Height:') + pokemonList[i].height + " " +
-  ('Type:') + pokemonList[i].type + '</br>');
-*/
+    {name:  "Squirtle",
+    height: 0.5,
+    type: ["Water"] }
+  ];
+
+  function getAll () {
+    return pokemonList;
+  }
+
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll
+    add: add
+  }
+
+})();
+
+pokemonRepository.forEach (function(pokemon)) {
+  document.write (pokemonList.getAll);
+};
+
+
+/*
 
 pokemonList.forEach (function(pokemon) {
   document.write('<br>' + pokemon.name + pokemon.height + pokemon.type + '</br>');
 });
 
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<br>' + pokemonList[i].name + " " +
+  ('Height:') + pokemonList[i].height + " " +
+  ('Type:') + pokemonList[i].type + '</br>');
 
-/* if (pokemonList[i].height > 0.5) {
+if (pokemonList[i].height > 0.5) {
   document.write('Wow, thats big!');
 };
+
 */
